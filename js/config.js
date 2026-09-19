2459de6f6a5e108498bae68b55b8f50338f21c68
Fábio Rocha <106245486+Fabiorocharb@users.noreply.github.com>
2026-09-19 11:21:07 -0300

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,12 @@
+﻿/* Preencha apenas com dados confirmados da profissional.
+   Sem WhatsApp/endereço, o site direciona ao Instagram já informado no projeto. */
+window.SITE_CONFIG = {
+  whatsapp: '5521983265333', // Formato internacional: 55 + DDD + número, apenas dígitos.
+  phoneDisplay: '+55 21 98326-5333',
+  email: '',
+  address: 'Center Francisco Gonçalves - R. Dr. Mattos, 44 - Sl 301 - Centro, Rio Bonito - RJ, 28800-000',
+  mapsUrl: 'https://www.google.com/maps/dir/?api=1&destination=' + encodeURIComponent('Center Francisco Gonçalves, R. Dr. Mattos, 44 - Centro, Rio Bonito - RJ, 28800-000'),
+  mapsEmbedUrl: 'https://www.google.com/maps?q=' + encodeURIComponent('Center Francisco Gonçalves, R. Dr. Mattos, 44 - Centro, Rio Bonito - RJ, 28800-000') + '&output=embed&hl=pt-BR&z=17',
+  instagram: 'https://www.instagram.com/brunapiresnutricionista/',
+  videos: { tea: '', seletividade: '', infancia: '' } // URLs de arquivos MP4/WebM.
+};
